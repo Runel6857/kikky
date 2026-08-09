45894ebf7552aef41b84bed56c135f341f9b7248
Runel6857 <domdomkursunu277@gmail.com>
2026-08-10 01:49:04 +0300

    Kikky java 21

--- src/main/java/de/johni0702/minecraft/bobby/BobbyConfigScreenFactory.java
+++ src/main/java/de/johni0702/minecraft/bobby/BobbyConfigScreenFactory.java
@@ -1,19 +1,9 @@
 package de.johni0702.minecraft.bobby;
 
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
-
-import java.util.function.Consumer;
 
 public class BobbyConfigScreenFactory {
-    public static Screen createConfigScreen(Screen parent, BobbyConfig config, Consumer<BobbyConfig> saveConsumer) {
-        ConfigBuilder builder = ConfigBuilder.create()
-                .setParentScreen(parent)
-                .setTitle(Text.literal("Bobby Settings"));
-
-        return builder.build();
+    public static Screen createConfigScreen(Screen parent, Object config, Object saveConsumer) {
+        return null;
     }
 }
--- src/main/java/de/johni0702/minecraft/bobby/BobbyModMenuApiImpl.java
+++ src/main/java/de/johni0702/minecraft/bobby/BobbyModMenuApiImpl.java
@@ -1,11 +1,4 @@
 package de.johni0702.minecraft.bobby;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-
-public class BobbyModMenuApiImpl implements ModMenuApi {
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> Bobby.getInstance().createConfigScreen(parent);
-    }
+public class BobbyModMenuApiImpl {
 }
